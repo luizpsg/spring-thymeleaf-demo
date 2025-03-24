0bc19307132f2a4fae0b04a0aaf31d5a0fa0ae11
Luiz Paulo Saud <luiz.saud@sga.pucminas.br>
2025-03-23 21:31:08 -0300

    Add StudentController and related templates for student registration

---
+++ src/main/java/com/luizpsg/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,43 @@
+package com.luizpsg.thymeleafdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.luizpsg.thymeleafdemo.model.Student;
+
+@Controller
+public class StudentController {
+
+  @Value("${countries}")
+  private List<String> countries;
+
+  @GetMapping("/showStudentForm")
+  public String showForm(Model theModel) {
+
+    // create a student object
+    Student theStudent = new Student();
+
+    // add student object to the model
+    theModel.addAttribute("student", theStudent);
+
+    // add the countries to the model
+    theModel.addAttribute("countries", countries);
+
+    return "student-form";
+  }
+
+  @PostMapping("/processStudentForm")
+  public String processForm(@ModelAttribute("student") Student theStudent) {
+
+    System.out.println("theStudent: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+    System.out.println("Country: " + theStudent.getCountry());
+
+    return "student-confirmation";
+  }
+}
